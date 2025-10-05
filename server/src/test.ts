6d660fb612694a0e14dc7127198de803961592d6
poocat <19279127+poocat@users.noreply.github.com>
2025-10-05 12:54:08 -0700

    randomize choices in "monkey" test

--- server/src/test.ts
+++ server/src/test.ts
@@ -6,30 +6,49 @@ import { GameData } from "@common/types";
 import { makeDecision } from "@server/game/stateMachine";
 import { createMockGameData } from "@server/mock";
 
+function shuffle(array: any[]) {
+  const copy = [...array];
+  let currentIndex = array.length;
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [copy[currentIndex], copy[randomIndex]] = [
+      copy[randomIndex], copy[currentIndex]];
+  }
+  return copy;
+}
+
 const mockGame = (initialGameData: GameData, numChoices: number) => {
   let gameData = initialGameData;
   for (let i = 0; i < numChoices; i++) {
     console.log(`${i} --------------------------------------------------`);
+    console.log(`activity:`, JSON.stringify(gameData.activity, null, 2));
     const currentChoice = gameData.activity.currentChoice;
-    console.log(`choice:`, currentChoice);
+    const randomizedValues = shuffle(currentChoice.values);
     // Always choose the maximum number of choices allowed from the beginning of
     // the list.
     const decision = {
       name: currentChoice.name,
       playerId: gameData.activity.currentChoice.choosingPlayerId,
-      values: currentChoice.values.slice(
+      values: randomizedValues.slice(
         0,
-        currentChoice.max ?? currentChoice.values.length,
+        currentChoice.max ?? randomizedValues.length,
       ),
     };
-    console.log(`decision:`, decision);
-    gameData = makeDecision({
-      gameData: gameData,
-      decision: decision,
-    });
+    console.log(`decision:`, JSON.stringify(decision, null, 2));
+    try {
+      gameData = makeDecision({
+        gameData: gameData,
+        decision: decision,
+      });
+    } catch (error) {
+      console.log(`Final game state ---------------------------------------------`);
+      console.log(JSON.stringify(gameData, null, 2));
+      throw error;
+    }
   }
   console.log(`Final game state ---------------------------------------------`);
   console.log(JSON.stringify(gameData, null, 2));
 };
 
-mockGame(createMockGameData(), 20);
+mockGame(createMockGameData(), 100);
