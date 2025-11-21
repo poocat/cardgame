fe4cf8415ca5078f32cab14de449bfa2a6822fe6
poocat <19279127+poocat@users.noreply.github.com>
2025-11-21 09:00:48 -0800

    fix bug in logger creation

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,25 +1,24 @@
 import { pino, Logger } from "pino";
 import { CONFIG } from "@server/config";
 
-const isDevEnv = CONFIG.nodeEnv === "development";
-
 export const logger: Logger = pino({
   level: process.env.LOG_LEVEL || "info",
   formatters: {
     level: (label) => ({ level: label.toUpperCase() }),
   },
-  transport: isDevEnv
-    ? {
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-          translateTime: "SYS:standard",
-          ignore: "pid,hostname",
-        },
-      }
-    : undefined,
+  transport:
+    CONFIG.nodeEnv === "development"
+      ? {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            translateTime: "SYS:standard",
+            ignore: "pid,hostname",
+          },
+        }
+      : undefined,
   base: {
-    env: isDevEnv || "development",
+    env: CONFIG.nodeEnv || "development",
   },
 });
 
