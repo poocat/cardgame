81ee72866d54bad1b34aebe84e15c8be99417f29
poocat <19279127+poocat@users.noreply.github.com>
2026-03-10 09:29:47 -0700

    add annotations to actions in digest

--- server/src/api/transformers.ts
+++ server/src/api/transformers.ts
@@ -102,6 +102,14 @@ export function digestGameData({
   const otherPlayerData = gameData.players.filter((p) => p.id !== playerId);
   const observingPlayerData = gameData.players.find((p) => p.id === playerId);
 
+  // Construct an annotation map.
+  const annotationMap: Record<Id, string[]> = {};
+  gameData.annotations.forEach(({ id, message }) => {
+    const match = annotationMap[id];
+    if (match) match.push(message);
+    else annotationMap[id] = [message];
+  });
+
   /** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
    * Use to create digests for cards that are visible to the observing player.
    ** * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -114,8 +122,14 @@ export function digestGameData({
     for (const type of actionTypes) {
       const match = cardActions.find((a) => a.type === type);
       if (match)
-        actions.push({ id: match.id, type, instructions: match.instructions });
+        actions.push({
+          id: match.id,
+          type,
+          instructions: match.instructions,
+          annotations: annotationMap[match.id] ?? [],
+        });
     }
+
     return {
       id: cardData.id,
       name: cardData.name,
