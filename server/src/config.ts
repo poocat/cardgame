72444522749f4aed395bfb4524bc6a0053135a3f
poocat <19279127+poocat@users.noreply.github.com>
2026-04-29 16:48:08 -0700

    switch to new host port for api

--- server/src/config.ts
+++ server/src/config.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const CONFIG = {
-  port: process.env.PORT || 7000,
+  port: process.env.PORT || 7070,
   nodeEnv: process.env.NODE_ENV || "development",
   mongoDbName: process.env.MONGODB_NAME || "cardgame",
   mongoDbUri: process.env.MONGODB_URI || `mongodb://localhost:27017`,
