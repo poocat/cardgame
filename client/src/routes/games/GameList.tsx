5dc3e4669f63c5d846dbe4cad7c09c5ae45c1854
poocat <19279127+poocat@users.noreply.github.com>
2026-05-08 12:17:31 -0700

    apply query hook

--- client/src/routes/games/GameList.tsx
+++ client/src/routes/games/GameList.tsx
@@ -1,47 +1,42 @@
+import { Box, Stack } from "@client/components";
+import { useApiQuery } from "@client/utils/api";
 import { ROUTES } from "@common/api/routes";
-import { useEffect, useState } from "react";
 import { Link } from "react-router";
-import type { z } from "zod";
-
-type GamesGetManyResponseBody = z.infer<
-  typeof ROUTES.games.methods.getMany.schemas.responseBody
->;
 
 export const GameList = () => {
-  const [error, setError] = useState<string | null>(null);
-  const [data, setData] = useState<GamesGetManyResponseBody | null>(null);
+  const url = `${ROUTES.games.path}`;
 
-  useEffect(() => {
-    const fetchGameList = async () => {
-      try {
-        const response = await fetch(ROUTES.games.path, {
-          method: "GET",
-        });
-        if (response.ok) {
-          const j = await response.json();
-          const d = ROUTES.games.methods.getMany.schemas.responseBody.parse(j);
-          setData(d);
-        } else {
-          setError(`Could not fetch games: ${response.statusText}`);
-        }
-      } catch (error) {
-        setError(`Error: ${error}`);
-      }
-    };
-    if (data === null && error === null) {
-      fetchGameList();
-    }
-  }, [data, error]);
+  const { data, error } = useApiQuery({
+    key: url,
+    fetch: async (signal) => {
+      const response = await fetch(url, { method: "GET", signal });
+      if (!response.ok)
+        throw new Error(`HTTP ${response.status} (${response.statusText})`);
+      return response;
+    },
+    parse: async (response) => {
+      const json = await response.json();
+      return ROUTES.games.methods.getMany.schemas.responseBody.parse(json);
+    },
+  });
 
+  if (error) {
+    return (
+      <Box spacing="md" color="error">
+        {error.message}
+      </Box>
+    );
+  }
   return (
-    <div>
-      <div>Games:</div>
-      {error && <div>{error}</div>}
-      {data?.games?.map((g) => (
-        <div key={g.gameId}>
-          <Link to={`/games/${g.gameId}`}>{g.gameId}</Link>
-        </div>
-      ))}
-    </div>
+    <Box spacing="lg">
+      <Stack orientation="vertical" spacing="md">
+        {data?.games?.map((g) => (
+          <Box key={g.gameId} spacing="sm" size="md" border="dark">
+            <Link to={`/games/${g.gameId}`}>{g.gameId}</Link>
+            <div>Last updated at: {g.updatedAt}</div>
+          </Box>
+        ))}
+      </Stack>
+    </Box>
   );
 };
