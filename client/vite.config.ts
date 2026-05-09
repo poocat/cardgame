cec435c718a9b6fe3ca8adebfbde3941901a4643
Steven Lewis <19279127+poocat@users.noreply.github.com>
2026-05-09 11:24:11 -0700

    Add App Title and Favicon (#70)
    In preparation for deploying the client app:
    - Removed the default Vite favicon, replaced with placeholder
    - Updated config to use alternative `public/` folder when available
    - Updated private submodule pointer to commit with alternative `public/`
    folder
    - Added an environment-configurable app title

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,17 @@
+import fs from "node:fs";
 import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const defaultPublicDir = path.resolve(__dirname, "./public");
+const privatePublicDir = path.resolve(__dirname, "../private/client/public");
+
 // https://vite.dev/config/
 export default defineConfig({
+  // Use alternative "public" folder in the private submodule if it exists.
+  publicDir: fs.existsSync(privatePublicDir)
+    ? privatePublicDir
+    : defaultPublicDir,
   resolve: {
     alias: {
       "@common": path.resolve(__dirname, "../common"),
