e1a723768b512b287c313543f7fcf00dc1936441
Steven Lewis <19279127+poocat@users.noreply.github.com>
2026-05-17 20:31:11 -0700

    Loading Indicators (#80)
    Added simple loading indicators ("Loading...") to "about" and "rulebook"
    pages.

--- client/src/routes/rulebook/Rulebook.tsx
+++ client/src/routes/rulebook/Rulebook.tsx
@@ -6,7 +6,7 @@ import { ROUTES } from "@common/api/routes";
 export const Rulebook = () => {
   const url = apiUrl(`${ROUTES.copy.path}/rules`);
 
-  const { data, error } = useApiQuery({
+  const { data, loading, error } = useApiQuery({
     key: url,
     fetch: async (signal) => {
       const response = await fetch(url, { method: "GET", signal });
@@ -18,6 +18,7 @@ export const Rulebook = () => {
     },
   });
 
+  if (loading) return <Box spacing="lg">Loading...</Box>;
   if (error) return <Box spacing="lg">Rules are not available.</Box>;
   if (!data) return null;
 
